5e81685b41c2af8ce871586a214541f08206e174
Linmay-29 <patillinmay1234@gmail.com>
2025-12-16 16:44:13 +0530

    Day1

---
+++ Day1/Day1_Daimond_Pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Day1_Daimond_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    if (j == 1 || j == 2 * i - 1)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    if (j == 1 || j == 2 * i - 1)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
